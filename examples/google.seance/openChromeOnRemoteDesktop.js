5347d37e76aeceae9432f15a4724e00c0eb53280
Ben Babik <Ben Babik>
2017-03-07 11:59:25 +0000

    tools for capture

--- examples/google.seance/openChromeOnRemoteDesktop.js
+++ examples/google.seance/openChromeOnRemoteDesktop.js
@@ -3,9 +3,11 @@
 
 with (mediums.vnc) {
     
-    connect("curlyben")
-    //connect("msedgewin10")
+    //connect("curlyben")
+    connect("msedgewin10")
    // connect("192.168.0.31")
+    
+    //connect("10.210.138.155")
     
     doubleclick("chromeicon.png")
 
